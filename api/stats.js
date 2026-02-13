c9b1b8aa07028cd54cdff457918c54e77d7e926c
elmilouaysaad <s.elmilouay@aui.ma>
2026-02-13 21:00:12 +0100

    second

--- api/stats.js
+++
@@ -1,7 +0,0 @@
-const { readVotes, computeStats } = require("./_store");
-
-module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  const data = readVotes();
-  return res.json({ ok: true, stats: computeStats(data.votes) });
-};
